a2ff9fe04f5deb57994e3afb7696cb91dfd2726e
Raul Espinosa <raul.espinosa@beora.com>
2020-03-31 19:01:20 -0600

    first commit

---
+++ lib/active_campaign_rails.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require 'active_campaign_rails/version'
+
+module ActiveCampaignRails
+  class Error < StandardError; end
+end
---
+++ lib/active_campaign_rails/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ActiveCampaignRails
+  VERSION = '0.1.0'
+end
